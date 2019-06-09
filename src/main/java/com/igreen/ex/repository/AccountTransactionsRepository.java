9dfd117b8a41546bc0f8d761334dd684f431fca3
Sindhu Harish <sindhu16rose@gmail.com>
2019-06-09 16:44:46 +1000

    repo class commit

---
+++ src/main/java/com/igreen/ex/repository/AccountTransactionsRepository.java
@@ -0,0 +1,23 @@
+/**
+ * @author Sindhu
+ * Date: 9-June-2019
+ */
+package com.igreen.ex.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.igreen.ex.model.AccountTransactionDetails;
+/**
+ * 
+ * AccountAccountDetails table Repository file
+ *
+ */
+@RepositoryRestResource
+public interface AccountTransactionsRepository extends CrudRepository<AccountTransactionDetails, Long>
+{
+	List<AccountTransactionDetails> findByAccountNumber(@Param("accountNumber") Long accountNumber);
+}
